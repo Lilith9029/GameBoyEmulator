10f0508d8976685aad9b5a41bbaaaa652aa3f2dc
Lilith <duongd4rk9029@gmail.com>
2026-03-26 18:57:28 +0700

    feat: implement 16-bit arithmetic / logical instructions feat: Misc / control instructions (Placeholder for Prefix)

--- GameBoyEmulator/CPU.cs
+++ GameBoyEmulator/CPU.cs
@@ -1,12 +1,9 @@
-﻿using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Runtime.Intrinsics.X86;
-
-public class CPU
+﻿public class CPU
 {
     // 8-bit registers
     public byte A, B, C, D, E, H, L;
     private byte _f;
+
     public byte F
     {
         get => _f;
@@ -110,439 +107,15 @@ public class CPU
 
     public int ExecuteNext()
     {
-        byte opcode = _mmu.Read(PC++);
-
+        byte opcode = Fetch();
+        
         switch (opcode)
         {
-            case 0x01: // LD BC, n16
-                return LD_rr_nn(v => BC = v);
-            case 0x02: // LD (BC), A
-                return LD_rr_A(BC);
-            case 0x04: // INC B
-                return INC_r(ref B);
-            case 0x05: // DEC B
-                return DEC_r(ref B);
-            case 0x06: // LD B, n8
-                return LD_r_n(ref B);
-            case 0x0A: // LD A, (BC)
-                return LD_A_rr(BC);
-            case 0x0C: // INC C
-                return INC_r(ref C);
-            case 0x0D: // DEC C
-                return DEC_r(ref C);
-            case 0x0E: // LD C, n8
-                return LD_r_n(ref C);
-            case 0x11: // LD DE, n16
-                return LD_rr_nn(v => DE = v);
-            case 0x12: // LD (DE), A
-                return LD_rr_A(DE);
-            case 0x14: // INC D
-                return INC_r(ref D);
-            case 0x15: // DEC D
-                return DEC_r(ref D);
-            case 0x16: // LD D, n8
-                return LD_r_n(ref D);
-            case 0x18: // JR e8
-                return JR_e8();
-            case 0x1A: // LD A, (DE)
-                return LD_A_rr(DE);
-            case 0x1C: // INC E
-                return INC_r(ref E);
-            case 0x1D: // DEC E
-                return DEC_r(ref E);
-            case 0x1E: // LD E, n8
-                return LD_r_n(ref E);
-            case 0x20: // JR NZ, e8
-                return JR_NZ_e8();
-            case 0x21: // LD HL, n16
-                return LD_rr_nn(v => HL = v);
-            case 0x22: // LD (HL+), A
-                return LD_HLI_A();
-            case 0x24: // INC H
-                return INC_r(ref H);
-            case 0x25: // DEC H
-                return DEC_r(ref H);
-            case 0x26: // LD H, n8
-                return LD_r_n(ref H);
-            case 0x27: // DDA
-                return DAA();
-            case 0x28: // JR Z, e8
-                return JR_Z_e8();
-            case 0x2A: // LD A, (HL+)
-                return LD_A_HLI();
-            case 0x2C: // INC L
-                return INC_r(ref L);
-            case 0x2D: // DEC L
-                return DEC_r(ref L);
-            case 0x2E: // LD L, n8
-                return LD_r_n(ref L);
-            case 0x2F: // CPL
-                return CPL();
-            case 0x30: // JR NC, e8
-                return JR_NC_e8();
-            case 0x31: // LD SP, n16
-                return LD_rr_nn(v => SP = v);
-            case 0x32: // LD (HL-), A
-                return LD_HLD_A();
-            case 0x34: // INC (HL)
-                return INC_HL();
-            case 0x35: // DEC (HL)
-                return DEC_HL();
-            case 0x36: // LD (HL), n8
-                return LD_HL_n();
-            case 0x37: // SCF
-                return SCF();
-            case 0x38: // JR C, e8
-                return JR_C_e8();
-            case 0x3A: // LD A, (HL-)
-                return LD_A_HLD();
-            case 0x3C: // INC A
-                return INC_r(ref A);
-            case 0x3D: // DEC A
-                return DEC_r(ref A);
-            case 0x3E: // LD A, n8
-                return LD_r_n(ref A);
-            case 0x3F: // CCF
-                return CCF();
-            case 0x40: // LD B, B
-                return LD_r_r(ref B, B);
-            case 0x41: // LD B, C
-                return LD_r_r(ref B, C);
-            case 0x42: // LD B, D
-                return LD_r_r(ref B, D);
-            case 0x43: // LD B, E
-                return LD_r_r(ref B, E);
-            case 0x44: // LD B, H
-                return LD_r_r(ref B, H);
-            case 0x45: // LD B, L
-                return LD_r_r(ref B, L);
-            case 0x46: // LD B, (HL)
-                return LD_r_HL(ref B);
-            case 0x47: // LD B, A
-                return LD_r_r(ref B, A);
-            case 0x48: // LD C, B
-                return LD_r_r(ref C, B);
-            case 0x49: // LD C, C
-                return LD_r_r(ref C, C);
-            case 0x4A: // LD C, D
-                return LD_r_r(ref C, D);
-            case 0x4B: // LD C, E
-                return LD_r_r(ref C, E);
-            case 0x4C: // LD C, H
-                return LD_r_r(ref C, H);
-            case 0x4D: // LD C, L
-                return LD_r_r(ref C, L);
-            case 0x4E: // LD C, (HL)
-                return LD_r_HL(ref C);
-            case 0x4F: // LD C, A
-                return LD_r_r(ref C, A);
-            case 0x50: // LD D, B
-                return LD_r_r(ref D, B);
-            case 0x51: // LD D, C
-                return LD_r_r(ref D, C);
-            case 0x52: // LD D, D
-                return LD_r_r(ref D, D);
-            case 0x53: // LD D, E
-                return LD_r_r(ref D, E);
-            case 0x54: // LD D, H
-                return LD_r_r(ref D, H);
-            case 0x55: // LD D, L
-                return LD_r_r(ref D, L);
-            case 0x56: // LD D, (HL)
-                return LD_r_HL(ref D);
-            case 0x57: // LD D, A
-                return LD_r_r(ref D, A);
-            case 0x58: // LD E, B
-                return LD_r_r(ref E, B);
-            case 0x59: // LD E, C
-                return LD_r_r(ref E, C);
-            case 0x5A: // LD E, D
-                return LD_r_r(ref E, D);
-            case 0x5B: // LD E, E
-                return LD_r_r(ref E, E);
-            case 0x5C: // LD E, H
-                return LD_r_r(ref E, H);
-            case 0x5D: // LD E, L
-                return LD_r_r(ref E, L);
-            case 0x5E: // LD E, (HL)
-                return LD_r_HL(ref E);
-            case 0x5F: // LD E, A
-                return LD_r_r(ref E, A);
-            case 0x60: // LD H, B
-                return LD_r_r(ref H, B);
-            case 0x61: // LD H, C
-                return LD_r_r(ref H, C);
-            case 0x62: // LD H, D
-                return LD_r_r(ref H, D);
-            case 0x63: // LD H, E
-                return LD_r_r(ref H, E);
-            case 0x64: // LD H, H
-                return LD_r_r(ref H, H);
-            case 0x65: // LD H, L
-                return LD_r_r(ref H, L);
-            case 0x66: // LD H, (HL)
-                return LD_r_HL(ref H);
-            case 0x67: // LD H, A
-                return LD_r_r(ref H, A);
-            case 0x68: // LD L, B
-                return LD_r_r(ref L, B);
-            case 0x69: // LD L, C
-                return LD_r_r(ref L, C);
-            case 0x6A: // LD L, D
-                return LD_r_r(ref L, D);
-            case 0x6B: // LD L, E
-                return LD_r_r(ref L, E);
-            case 0x6C: // LD L, H
-                return LD_r_r(ref L, H);
-            case 0x6D: // LD L, L
-                return LD_r_r(ref L, L);
-            case 0x6E: // LD L, (HL)
-                return LD_r_HL(ref L);
-            case 0x6F: // LD L, A
-                return LD_r_r(ref L, A);
-            case 0x70: // LD (HL), B
-                return LD_HL_r(ref B);
-            case 0x71: // LD (HL), C
-                return LD_HL_r(ref C);
-            case 0x72: // LD (HL), D
-                return LD_HL_r(ref D);
-            case 0x73: // LD (HL), E
-                return LD_HL_r(ref E);
-            case 0x74: // LD (HL), H
-                return LD_HL_r(ref H);
-            case 0x75: // LD (HL), L
-                return LD_HL_r(ref L);
-            case 0x77: // LD (HL), A
-                return LD_HL_r(ref A);
-            case 0x78: // LD A, B
-                return LD_r_r(ref A, B);
-            case 0x79: // LD A, C
-                return LD_r_r(ref A, C);
-            case 0x7A: // LD A, D
-                return LD_r_r(ref A, D);
-            case 0x7B: // LD A, E
-                return LD_r_r(ref A, E);
-            case 0x7C: // LD A, H
-                return LD_r_r(ref A, H);
-            case 0x7D: // LD A, L
-                return LD_r_r(ref A, L);
-            case 0x7E: // LD A, (HL)
-                return LD_r_HL(ref A);
-            case 0x7F: // LD A, A
-                return LD_r_r(ref A, A);
-            case 0x80: // ADD A, B
-                return ADD_A_r(B);
-            case 0x81: // ADD A, C
-                return ADD_A_r(C);
-            case 0x82: // ADD A, D
-                return ADD_A_r(D);
-            case 0x83: // ADD A, E
-                return ADD_A_r(E);
-            case 0x84: // ADD A, H
-                return ADD_A_r(H);
-            case 0x85: // ADD A, L
-                return ADD_A_r(L);
-            case 0x86: // ADD A, (HL)
-                return ADD_A_HL();
-            case 0x87: // ADD A, A
-                return ADD_A_r(A);
-            case 0x88: // ADC A, B
-                return ADC_A_r(B);
-            case 0x89: // ADC A, C
-                return ADC_A_r(C);
-            case 0x8A: // ADC A, D
-                return ADC_A_r(D);
-            case 0x8B: // ADC A, E
-                return ADC_A_r(E);
-            case 0x8C: // ADC A, H
-                return ADC_A_r(H);
-            case 0x8D: // ADC A, L
-                return ADC_A_r(L);
-            case 0x8E: // ADC A, (HL)
-                return ADC_A_HL();
-            case 0x8F: // ADC A, A
-                return ADC_A_r(A);
-            case 0x90: // SUB A, B
-                return SUB_A_r(B);
-            case 0x91: // SUB A, C
-                return SUB_A_r(C);
-            case 0x92: // SUB A, D
-                return SUB_A_r(D);
-            case 0x93: // SUB A, E
-                return SUB_A_r(E);
-            case 0x94: // SUB A, H
-                return SUB_A_r(H);
-            case 0x95: // SUB A, L
-                return SUB_A_r(L);
-            case 0x96: // SUB A, (HL)
-                return SUB_A_HL();
-            case 0x97: // SUB A, A
-                return SUB_A_r(A);
-            case 0x98: // SBC A, B
-                return SBC_A_r(B);
-            case 0x99: // SBC A, C
-                return SBC_A_r(C);
-            case 0x9A: // SBC A, D
-                return SBC_A_r(D);
-            case 0x9B: // SBC A, E
-                return SBC_A_r(E);
-            case 0x9C: // SBC A, H
-                return SBC_A_r(H);
-            case 0x9D: // SBC A, L
-                return SBC_A_r(L);
-            case 0x9E: // SBC A, (HL)
-                return SBC_A_HL();
-            case 0x9F: // SBC A, A
-                return SBC_A_r(A);
-            case 0xA0: // AND A, B
-                return AND_A_r(B);
-            case 0xA1: // AND A, C
-                return AND_A_r(C);
-            case 0xA2: // AND A, D
-                return AND_A_r(D);
-            case 0xA3: // AND A, E
-                return AND_A_r(E);
-            case 0xA4: // AND A, H
-                return AND_A_r(H);
-            case 0xA5: // AND A, L
-                return AND_A_r(L);
-            case 0xA6: // AND A, (HL)
-                return AND_A_HL();
-            case 0xA7: // AND A, A
-                return AND_A_r(A);
-            case 0xA8: // XOR A, B
-                return XOR_A_r(B);
-            case 0xA9: // XOR A, C
-                return XOR_A_r(C);
-            case 0xAA: // XOR A, D
-                return XOR_A_r(D);
-            case 0xAB: // XOR A, E
-                return XOR_A_r(E);
-            case 0xAC: // XOR A, H
-                return XOR_A_r(H);
-            case 0xAD: // XOR A, L
-                return XOR_A_r(L);
-            case 0xAE: // XOR A, (HL)
-                return XOR_A_HL();
-            case 0xAF: // XOR A, A
-                return XOR_A_r(A);
-            case 0xB0: // OR A, B
-                return OR_A_r(B);
-            case 0xB1: // OR A, C
-                return OR_A_r(C);
-            case 0xB2: // OR A, D
-                return OR_A_r(D);
-            case 0xB3: // OR A, E
-                return OR_A_r(E);
-            case 0xB4: // OR A, H
-                return OR_A_r(H);
-            case 0xB5: // OR A, L
-                return OR_A_r(L);
-            case 0xB6: // OR A, (HL)
-                return OR_A_HL();
-            case 0xB7: // OR A, A
-                return OR_A_r(A);
-            case 0xB8: // CP A, B
-                return CP_A_r(B);
-            case 0xB9: // CP A, C
-                return CP_A_r(C);
-            case 0xBA: // CP A, D
-                return CP_A_r(D);
-            case 0xBB: // CP A, E
-                return CP_A_r(E);
-            case 0xBC: // CP A, H
-                return CP_A_r(H);
-            case 0xBD: // CP A, L
-                return CP_A_r(L);
-            case 0xBE: // CP A, (HL)
-                return CP_A_HL();
-            case 0xBF: // CP A, A
-                return CP_A_r(A);
-            case 0xC0: // RET NZ
-                return RET_NZ();
-            case 0xC1: // POP BC
-                return POP_rr(ref B, ref C);
-            case 0xC2: // JP NZ, a16
-                return JP_NZ_a16();
-            case 0xC3: // JP a16
-                return JP_a16();
-            case 0xC4: // CALL NZ, a16
-                return CALL_NZ_a16();
-            case 0xC5: // PUSH BC
-                return PUSH_rr(ref B, ref C);
-            case 0xC6: // ADD A, n8
-                return ADD_A_n8();
-            case 0xC8: // RET Z
-                return RET_Z();
-            case 0xC9: // RET
-                return RET();
-            case 0xCA: // JP Z, a16
-                return JP_Z_a16();
-            case 0xCC: // CALL Z, a16
-                return CALL_Z_a16();
-            case 0xCD: // CALL a16
-                return CALL_a16();
-            case 0xCE: // ADC A, n8
-                return ADC_A_n8();
-            case 0xD0: // RET NC
-                return RET_NC();
-            case 0xD1: // POP DE
-                return POP_rr(ref D, ref E);
-            case 0xD2: // JP NC, a16
-                return JP_NC_a16();
-            case 0xD4: // CALL NC, a16
-                return CALL_NC_a16();
-            case 0xD5: // PUSH DE
-                return PUSH_rr(ref D, ref E);
-            case 0xD6: // SUB A, n8
-                return SUB_A_n8();
-            case 0xD8: // RET C
-                return RET_C();
-            case 0xD9: // RETI
-                return RETI();
-            case 0xDA: // JP C, a16
-                return JP_C_a16();
-            case 0xDC: // CALL C, a16
-                return CALL_C_a16();
-            case 0xDE: // SBC A, n8
-                return SBC_A_n8();
-            case 0xE0: // LDH (a8), A
-                return LDH_a8_A();
-            case 0xE1: // POP HL
-                return POP_rr(ref H, ref L);
-            case 0xE2: // LDH (C), A
-                return LDH_C_A();
-            case 0xE5: // PUSH HL
-                return PUSH_rr(ref H, ref L);
-            case 0xE6: // AND A, n8
-                return AND_A_n8();
-            case 0xE9: // JP HL
-                return JP_HL();
-            case 0xEA: // LD (a16), A
-                return LD_a16_A();
-            case 0xEE: // XOR A, n8
-                return XOR_A_n8();
-            case 0xF0: // LDH A, (a8)
-                return LDH_A_a8();
-            case 0xF1: // POP AF
-                return POP_AF();
-            case 0xF2: // LDH A, (C)
-                return LDH_A_C();
-            case 0xF5: // PUSH AF
-                return PUSH_AF();
-            case 0xF6: // OR A, n8
-                return OR_A_n8();
-            case 0xF8: // LD HL, SP+e8
-                return LD_HL_SP_e8();
-            case 0xF9: // LD SP, HL
-                return LD_SP_HL();
-            case 0xFA: // LD A, (a16)
-                return LD_A_a16();
-            case 0xFE: // CP A, n8
-                return CP_A_n8();
+            case 0x00:
+                return NOP();
             default:
                 Console.WriteLine($"Unknown opcode: 0x{opcode:X2} at PC: 0x{PC - 1:X4}");
-                return 4;
+                return 4;       
         }
     }
 
@@ -1240,4 +813,84 @@ public class CPU
         FlagC = !FlagC;
         return 4;
     }
+
+    // 16-bit arithmetic / logical instructions
+    private int INC_rr(Func<ushort> get, Action<ushort> set)
+    {
+        ushort value = get();
+        value++;
+        set(value);
+        // set((ushort)(get() + 1));
+        return 8; 
+    }
+
+    private int ADD_HL_rr(ushort rr)
+    {
+        int result = HL + rr;
+        FlagN = false;
+        FlagH = ((HL & 0x0FFF) + (rr & 0x0FFF)) > 0x0FFF;
+        FlagC = result > 0xFFFF;
+        HL = (ushort)result;
+        return 8;
+    }
+
+    private int DEC_rr(Func<ushort> get, Action<ushort> set)
+    {
+        ushort value = get();
+        value--;
+        set(value);
+        // set((ushort)(get() - 1));
+        return 8;
+    }
+
+    private int ADD_SP_e8()
+    {
+        sbyte e8 = (sbyte)Fetch();
+        int result = SP + e8;
+        FlagZ = false;
+        FlagN = false;
+        FlagH = ((SP & 0x0F) + (e8 & 0x0F)) > 0x0F;
+        FlagC = ((SP & 0xFF) + (e8 & 0xFF)) > 0xFF;
+        SP = (ushort)result;
+        return 16;
+    }
+
+    // Misc / control instructions
+    private int NOP()
+    {
+        return 4;   
+    }
+
+    private int STOP()
+    {
+        PC++;
+        return 4;
+    }
+
+    private int HALT()
+    {
+        _halted = true;
+        return 4;
+    }
+
+    private int CB_PREFIX()
+    {
+        byte cbOpcode = Fetch();
+        Console.WriteLine($"CB opcode not implemented: 0x{cbOpcode:X2}");
+        return 8;
+    }
+
+    private int DI()
+    {
+        _ime = false;
+        return 4;
+    }
+
+    private int EI()
+    {
+        _ime = true;
+        return 4;
+    }   
+
+    // 8-bit shift, rotate and bit instructions
 }
